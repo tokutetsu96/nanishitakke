3a6c10834e7ea1c16a52be0b1cd103ea6f48f6e7
tokutetsu96 <tokunagatetsuya96@gmail.com>
2025-12-28 19:53:35 +0900

    ビルドエラーを回避

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import { ReactNode } from 'react';
+import type { ReactNode } from 'react';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { session, loading } = useAuth();
